e9761319c5fb306d359e866e967ef408cbfddcbe
Zachary <bennett.zachary@outlook.com>
2018-05-10 21:05:29 -0700

    refactored skill columns into a component

---
+++ src/components/skill-column.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default ({containerClass, iconPath, name, skills}) => (
+    <div className={containerClass}>
+        <img src={iconPath}/>
+        <h3>{name}</h3>
+        <p></p>
+
+        <br/>
+
+        <ul>
+            {skills.map(skill => <li>{skill}</li>)}
+        </ul>
+    </div>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,38 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import HeroImage from '../components/hero-image';
+import SkillColumn from '../components/skill-column';
 
 import frontEndBadgePath from '../../images/javascript-badge.png';
 import backEndBadgePath from '../../images/gears.png';
 import devOpsBadgePath from '../../images/tools.png';
 
-const IndexPage = () => (
-  <div>
-    <h1 className="title">Zachary Bennett</h1>
-    <h3 className="sub-title">Full-Stack Software Engineer</h3>
-    <HeroImage/>
-    <div className="tag-line is-aqua">
-      <h1>Passion For People. Passion For Code.</h1>
-      <p>An AWS Certified Developer Associate, Javascript afficionado, passionate problem solver and communicator, I love to pioneer innovative software solutions by coupling my unique <em>'big picture'</em> perspective with masterful attention to detail.</p>
-      <h3>My source code is my <em>art</em>.</h3>
-    </div>
-    <div className="dev-info-grid">
-      <div className="front-end-info">
-        <img src={frontEndBadgePath}/>
-        <h3>Front-End</h3>
-        <p></p>
-        <br/>
+const IndexPage = () => {
+  const skills = {
+     frontEnd: ["Vanilla JavaScript", "React", "Angular", "Gatsby", "Wordpress", "CSS", "SCSS", "HTML"],
+      backEnd: ["Node","Scala","C#/Java","Go","SQL","MongoDB","DynamoDB","S3","Nginx"],
+      devOps: ["Docker","Jenkins","Ansible", "ELK Stack"]
+  };
 
-        <ul>
-          <li>Vanilla JavaScript</li>
-          <li>React</li> 
-          <li>Angular</li> 
-          <li>Gatsby</li> 
-          <li>Wordpress</li> 
-          <li>CSS</li> 
-          <li>SCSS</li> 
-          <li>HTML</li> 
+  return (
+      <div>
+        <h1 className="title">Zachary Bennett</h1>
+        <h3 className="sub-title">Full-Stack Software Engineer</h3>
+        <HeroImage/>
+        <div className="tag-line is-aqua">
+          <h1>Passion For People. Passion For Code.</h1>
+          <p>An AWS Certified Developer Associate, Javascript afficionado, passionate problem solver and communicator, I love to pioneer innovative software solutions by coupling my unique <em>'big picture'</em> perspective with masterful attention to detail.</p>
+          <h3>My source code is my <em>art</em>.</h3>
+        </div>
+        <div className="dev-info-grid">
+          <SkillColumn containerClass="front-end-info" iconPath={frontEndBadgePath} name="Front-End" skills={skills.frontEnd}/>
+          <SkillColumn containerClass="back-end-info" iconPath={backEndBadgePath} name="Back-End" skills={skills.backEnd}/>
+          <SkillColumn containerClass="dev-ops-info" iconPath={devOpsBadgePath} name="DevOps" skills={skills.devOps}/>
+        </div>
 
-        </ul>
+        {/* <Link to="/page-2/">Page 2</Link> */}
       </div>
-      <div className="back-end-info">
-        <img src={backEndBadgePath}/>
-        <h3>Back-End</h3>
-        <p></p>
-        
-        <br/>
-
-        <ul>
-          <li>Node</li> 
-          <li>Scala</li> 
-          <li>C#/Java</li>
-          <li>Go</li> 
-          <li>SQL</li> 
-          <li>MongoDB</li> 
-          <li>DynamoDB</li> 
-          <li>S3</li> 
-          <li>Nginx</li>
-        </ul>
-      </div>
-      <div className="dev-ops-info">
-        <img src={devOpsBadgePath}/>
-        <h3>DevOps</h3>
-        <p></p>
-        
-        <br/>
-
-        <ul>
-          <li>Docker</li> 
-          <li>Jenkins</li> 
-          <li>Ansible</li> 
-        </ul>
-      </div>
-    </div>
-
-    {/* <Link to="/page-2/">Page 2</Link> */}
-  </div>
-)
+    )
+}
 
 export default IndexPage
